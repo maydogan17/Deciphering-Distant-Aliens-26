39a152815bac2681d60f1a10abae283c544e4796
maydogan17 <83009554+maydogan17@users.noreply.github.com>
2021-05-10 23:04:56 +0300

    feat(core): add subs & firstTry

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,47 @@
 package main;
-//Burhan Özer Çavdar
-public class Main {
 
+public class Main {
+	
+	int ozer[] = {4, 1, 5};
+	int furkan[] = {3, 2, 5};
+	int gulay[] = {4, 8, 3};
+	
+	String words[] = {"BFBZD", "BBZF", "ZBZD"};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+	}
+	
+	public int[] firstTry(String in) {
+		int res[] = {0, 0, 0};
+		
+		String frs = words[0];
+		String sec = words[1];
+		String thr = words[2];
+		
+		for(int i = 0; i<in.length();i++) {
+			if(subs(in,i,frs)) {
+				
+			}
+		}
+		
+		
+		
+		return furkan;
+	}
+	
+	public boolean subs(String in, int begin, String compare) {
+		int end = compare.length();
+		boolean res = true;
+		
+		for(int i = 0;i < end;i++) {
+			if(in.charAt(begin + i) != compare.charAt(i)) {
+				res = false;
+			}
 
+		}
+		
+		return res;
 	}
 
 }
