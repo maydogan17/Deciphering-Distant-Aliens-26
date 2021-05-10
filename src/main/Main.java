0b4832765a052a040893bae01589060acff17f37
furkan-bulgur <81180218+furkan-bulgur@users.noreply.github.com>
2021-05-10 23:28:12 +0300

    feat(core): add first brute algorithm
    a brute force algorithm with complexity O(m*n*z) where m is the number of aliens, n is the length of the input, z is length of the alien name

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,41 +1,56 @@
 package main;
 
+import java.util.Arrays;
+
 public class Main {
 	
 	int ozer[] = {4, 1, 5};
 	int furkan[] = {3, 2, 5};
 	int gulay[] = {4, 8, 3};
 	
-	String words[] = {"BFBZD", "BBZF", "ZBZD"};
+	static String words[] = {"BFBZD", "BBZF", "ZBZD"};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world!");
+		String initText = "BBFFZBBFZZZBFBBZBZBBBBFFZFBFZZBBBZBFBBZBFFZZBBBBZZBBBFFBFBZDBFBZDBFBZDBFBZDZBZDZBZDZBZDZBZDZBZDBFBBBBFBFZBBBZBFZZBBBFBFZBBFBZBZZFBBZZBBZFFBBBBZBZZFZZFFFFBBBFFBFFFBZBFBBFFBBZFBBZF";
+		System.out.println(Arrays.toString(firstTry(initText))); 
+
 	}
 	
-	public int[] firstTry(String in) {
+	public static int[] firstTry(String in) {
 		int res[] = {0, 0, 0};
-		
-		String frs = words[0];
-		String sec = words[1];
-		String thr = words[2];
-		
-		for(int i = 0; i<in.length();i++) {
-			if(subs(in,i,frs)) {
-				
+
+		for(int j = 0; j<words.length;j++) {
+			String word = words[j];
+			int maxConsecCounter = 0;
+			int consecutiveCounter = 0;
+			
+			for(int i = 0; i<in.length();i++) {
+				if(checkIfNameMatch(in,i,word)) {
+					consecutiveCounter++;
+					if(consecutiveCounter>maxConsecCounter) {
+						maxConsecCounter = consecutiveCounter;
+					}
+					//ABCDHS 0 ABC 3
+					i += word.length() - 1;
+					
+				}else {
+					consecutiveCounter = 0;
+				}
 			}
+			res[j] = maxConsecCounter;
 		}
 		
 		
 		
-		return furkan;
+		return res;
 	}
 	
-	public boolean subs(String in, int begin, String compare) {
-		int end = compare.length();
+	public static boolean checkIfNameMatch(String in, int begin, String word) {
+		int end = word.length();
 		boolean res = true;
 		
 		for(int i = 0;i < end;i++) {
-			if(in.charAt(begin + i) != compare.charAt(i)) {
+			if(begin+i<in.length() && (in.charAt(begin + i) != word.charAt(i))) {
 				res = false;
 			}
 
