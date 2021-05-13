8b569f06898c40cab06db904df60a1d06090b3be
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2021-05-13 23:15:33 +0300

    feat(core): readCsv() added

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,10 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +13,9 @@ public class Main {
 	int ozer[] = {4, 1, 5};
 	int furkan[] = {3, 2, 5};
 	int gulay[] = {4, 8, 3};
+	static Map<String, ArrayList<Integer>> aliens = new HashMap<String, ArrayList<Integer>>();
+	static ArrayList<String> wordArrayList = new ArrayList<String>();
+	static ArrayList<String> lines = new ArrayList<String>();
 	static String words[] = {"BFBZD", "BBZF", "ZBZD"};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,6 +23,7 @@ public class Main {
 		//System.out.println(Arrays.toString(firstTry(initText))); 
 		String text = readText("1.txt");
 		System.out.println(Arrays.toString(firstTry(text))); 
+		readCSV("small_aliens.csv");
 	}
 	
 	public static int[] firstTry(String in) {
@@ -78,6 +85,45 @@ public class Main {
 		}
 		
 		return output;
+	}
+	
+	public static void readCSV(String fileName) {
+		Scanner sc;
+		try {
+			//this part reads the lines
+			sc = new Scanner(new File(fileName));
+			while (sc.hasNextLine()) 
+			{
+				lines.add(sc.nextLine()); 
+			}
+			sc.close(); // closes the scanner
+			
+			//this part parses among the lines and fill the alien map
+			Scanner lineScanner = null; 
+			for(int i=0; i<lines.size(); i++) {
+				lineScanner = new Scanner(lines.get(i));
+				lineScanner.useDelimiter(";"); // sets the delimiter pattern
+				if(i == 0) { //headers' line
+					lineScanner.next(); //pass the first word
+					while (lineScanner.hasNext()){
+						wordArrayList.add(lineScanner.next()); //add the words
+					}
+				}else {
+					String alienName = lineScanner.next();
+					ArrayList<Integer> values = new ArrayList<Integer>();
+					while (lineScanner.hasNext()) {
+						values.add(Integer.parseInt(lineScanner.next())); //add the values
+					}
+					aliens.put(alienName, values); //add the values read into the map
+				}
+			}
+			lineScanner.close();
+			System.out.println(wordArrayList);
+			System.out.println(aliens);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 	}
 
 }
