231ec4dbbdf47e020d5de8f48117f498d203324b
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2021-05-13 22:47:35 +0300

    feat(core): readText() added

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,22 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 	
 	int ozer[] = {4, 1, 5};
 	int furkan[] = {3, 2, 5};
 	int gulay[] = {4, 8, 3};
-	
 	static String words[] = {"BFBZD", "BBZF", "ZBZD"};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String initText = "BBFFZBBFZZZBFBBZBZBBBBFFZFBFZZBBBZBFBBZBFFZZBBBBZZBBBFFBFBZDBFBZDBFBZDBFBZDZBZDZBZDZBZDZBZDZBZDBFBBBBFBFZBBBZBFZZBBBFBFZBBFBZBZZFBBZZBBZFFBBBBZBZZFZZFFFFBBBFFBFFFBZBFBBFFBBZFBBZF";
-		System.out.println(Arrays.toString(firstTry(initText))); 
-
+		//String initText = "BBFFZBBFZZZBFBBZBZBBBBFFZFBFZZBBBZBFBBZBFFZZBBBBZZBBBFFBFBZDBFBZDBFBZDBFBZDZBZDZBZDZBZDZBZDZBZDBFBBBBFBFZBBBZBFZZBBBFBFZBBFBZBZZFBBZZBBZFFBBBBZBZZFZZFFFFBBBFFBFFFBZBFBBFFBBZFBBZF";
+		//System.out.println(Arrays.toString(firstTry(initText))); 
+		String text = readText("1.txt");
+		System.out.println(Arrays.toString(firstTry(text))); 
 	}
 	
 	public static int[] firstTry(String in) {
@@ -57,6 +60,24 @@ public class Main {
 		}
 		
 		return res;
+	}
+	
+	public static String readText(String fileName) {
+		String output = "";
+		try {
+			File file = new File(fileName);
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				String data = scan.nextLine();
+				output += data;
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		
+		return output;
 	}
 
 }
