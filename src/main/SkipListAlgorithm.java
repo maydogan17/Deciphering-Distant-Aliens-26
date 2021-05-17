9eef7b057d414e88f13a32273504a688c024ad26
Gülay Canbaloğlu <60276129+notreculi@users.noreply.github.com>
2021-05-17 23:49:47 +0300

    feat(core): SkipListAlgorithm added with a problem
    finds all words, not consecutive ones!!

---
+++ src/main/SkipListAlgorithm.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class SkipListAlgorithm extends Algorithm{
+	
+	
+	public SkipListAlgorithm(String text, Map<String, ArrayList<Integer>> aliensMap, ArrayList<String> wordList) {
+		super(text, aliensMap, wordList);
+		// TODO Auto-generated constructor stub
+		ArrayList<Integer> vars = new ArrayList<Integer>();
+		
+		SkiplistPrepareData data = new SkiplistPrepareData();
+		data.getSkiplists(text);
+		
+		for(int a=0; a<wordList.size(); a++) {
+			
+			String word = wordList.get(a);
+			int wordcount = 0;
+			int incount=0;
+			for(int i=0; i<text.length(); i++) {
+				
+				for(int j=0; j<word.length(); j++) {
+					if(i+j<text.length()) {
+						if(data.getSkiplist(word.charAt(j))[j+i] == 1) {
+							incount++;
+						}
+					}
+				}
+				if(incount==word.length()) {
+					wordcount++;
+				}
+				incount = 0;
+			}
+			
+		vars.add(wordcount);
+		}
+		System.out.println(vars);
+		// using for-each loop for iteration over Map.entrySet()
+        for (Map.Entry<String,ArrayList<Integer>> entry : aliensMap.entrySet()) {
+            if(entry.getValue().equals(vars)) System.out.println(entry.getKey());
+        }
+        
+	}
+
+	@Override
+	public String run() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/SkiplistPrepareData.java
+++ src/main/SkiplistPrepareData.java
@@ -3,70 +3,81 @@ package main;
 import java.util.Arrays;
 
 public class SkiplistPrepareData {
-		int[] skiplistB;
-		int[] skiplistD;
-		int[] skiplistF;
-		int[] skiplistZ;
-		
-		public void getSkiplists(String sampleText) {
-			ReadData readData = new ReadData();
-			String text = readData.readText(sampleText);
-			skiplistB = new int[text.length()];
-			skiplistD = new int[text.length()];
-			skiplistF = new int[text.length()];
-			skiplistZ = new int[text.length()];
-			
-			initializeSkipLists(text);
-			
-			for (int i = 0; i < text.length(); i++) {
-				
-				switch(text.charAt(i)) {
-				case 'B':
-					skiplistB[i] = 1;
-					break;
-				case 'D':
-					skiplistD[i] = 1;
-					break;
-				case 'F':
-					skiplistF[i] = 1;
-					break;
-				case 'Z':
-					skiplistZ[i] = 1;
-					break;
-				}
-				
+	int[] skiplistB;
+	int[] skiplistD;
+	int[] skiplistF;
+	int[] skiplistZ;
+
+	public void getSkiplists(String text) {
+		skiplistB = new int[text.length()];
+		skiplistD = new int[text.length()];
+		skiplistF = new int[text.length()];
+		skiplistZ = new int[text.length()];
+
+		initializeSkipLists(text);
+
+		for (int i = 0; i < text.length(); i++) {
+
+			switch (text.charAt(i)) {
+			case 'B':
+				skiplistB[i] = 1;
+				break;
+			case 'D':
+				skiplistD[i] = 1;
+				break;
+			case 'F':
+				skiplistF[i] = 1;
+				break;
+			case 'Z':
+				skiplistZ[i] = 1;
+				break;
+			default:
+				break;
 			}
-			
-			//System.out.println(Arrays.toString(skiplistB));
-		}
-		
-		
-		public void initializeSkipLists(String text) {
-			for (int i = 0; i < text.length(); i++) {
-				skiplistB[i] = 0;
-				skiplistD[i] = 0;
-				skiplistF[i] = 0;
-				skiplistZ[i] = 0;				
-			}
+
 		}
 
+		//System.out.println(Arrays.toString(skiplistZ));
+	}
 
-		public int[] getSkiplistB() {
+	public void initializeSkipLists(String text) {
+		for (int i = 0; i < text.length(); i++) {
+			skiplistB[i] = 0;
+			skiplistD[i] = 0;
+			skiplistF[i] = 0;
+			skiplistZ[i] = 0;
+		}
+	}
+
+	public int[] getSkiplist(char a) {
+		switch (a) {
+		case 'B':
 			return skiplistB;
-		}
-
-
-		public int[] getSkiplistD() {
+		case 'D':
 			return skiplistD;
-		}
-
-
-		public int[] getSkiplistF() {
+		case 'F':
 			return skiplistF;
-		}
-
-
-		public int[] getSkiplistZ() {
+		case 'Z':
 			return skiplistZ;
+		default:
+			return null;
 		}
+
+	}
+
+	public int[] getSkiplistB() {
+		return skiplistB;
+	}
+
+	public int[] getSkiplistD() {
+		return skiplistD;
+	}
+
+	public int[] getSkiplistF() {
+		return skiplistF;
+	}
+
+	public int[] getSkiplistZ() {
+		return skiplistZ;
+	}
 }
