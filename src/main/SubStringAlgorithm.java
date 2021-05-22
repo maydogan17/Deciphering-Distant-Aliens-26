55f3869d85dafba84ca327bbee250148cc4ca04a
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2021-05-22 19:24:56 +0300

    feat(core): SubStringAlgorithm completed

--- src/main/SubStringAlgorithm.java
+++ src/main/SubStringAlgorithm.java
@@ -3,11 +3,11 @@ package main;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class BitArrayAlgorithm extends Algorithm {
+public class SubStringAlgorithm extends Algorithm {
 	
 	private ArrayList<Integer> wordOccurences = new ArrayList<>();
 	
-	public BitArrayAlgorithm(String text, Map<String, ArrayList<Integer>> aliensMap, ArrayList<String> wordList) {
+	public SubStringAlgorithm(String text, Map<String, ArrayList<Integer>> aliensMap, ArrayList<String> wordList) {
 		super(text, aliensMap, wordList);
 		// TODO Auto-generated constructor stub
 	}
@@ -23,39 +23,47 @@ public class BitArrayAlgorithm extends Algorithm {
 		initValues();
 		ArrayList<Integer> occurList = new ArrayList<Integer>();
 		for(String word : wordList) {
-			int maxOccurenceForWord = 0;
+			int maxConsecForWord = 0;
 			for(int i=0; i<word.length(); i++) {
-				String bitArray = extractCombinationsFromIndex(word, i);
-				int bitParse = Integer.parseInt(bitArray, 2);
-				int maxConsecutive = maxConsecutiveOnes(bitParse);
-				if(maxConsecutive > maxOccurenceForWord) {
-					maxOccurenceForWord = maxConsecutive;
-				}		
+				int maxConsecForCombination = extractCombinationsFromIndex(word, i);
+				if(maxConsecForCombination > maxConsecForWord) maxConsecForWord = maxConsecForCombination;
+//				String bitArray = extractCombinationsFromIndex(word, i);
+//				int bitParse = Integer.parseInt(bitArray, 2);
+//				int maxConsecutive = maxConsecutiveOnes(bitParse);
+//				if(maxConsecutive > maxOccurenceForWord) {
+//					maxOccurenceForWord = maxConsecutive;
+//				}		
 			}
-			occurList.add(maxOccurenceForWord);
+			occurList.add(maxConsecForWord);
 		}
 		String result = findAlienNameFromMap(occurList);
 		return result;
 		
 	}
 	
-	private String extractCombinationsFromIndex(String word, int index) {
-		String bitString = "";
+	private int extractCombinationsFromIndex(String word, int index) {
+		int count = 0, maxConsec = 0;
 		for(int i=index; i<text.length(); i=i+word.length()) {
-			if(i+word.length() < text.length()) {
+			if(i+word.length() <= text.length()) {
 				String str = text.substring(i, i + word.length());
 				if (str.equals(word)) {
-					bitString += "1";
-				} else {
-					if(bitString.equals("") || bitString.endsWith("1")) {
-						bitString += "0";
-					}
+					count++;
+					if(maxConsec < count) maxConsec = count;
+				}else {
+					count = 0;
 				}
+//				if (str.equals(word)) {
+//					bitString += "1";
+//				} else {
+//					if(bitString.equals("") || bitString.endsWith("1")) {
+//						bitString += "0";
+//					}
+//				}
 			}
 			
 		}
-		System.out.println(bitString);
-		return bitString;
+
+		return maxConsec;
 	}
 	
 	private static int maxConsecutiveOnes(int x) {
