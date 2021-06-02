20fe240a664fee92674a11de9966112e755adf3d
maydogan17 <83009554+maydogan17@users.noreply.github.com>
2021-06-02 22:02:12 +0300

    fix(core): clear old bitarray function from skiplistpreparedata

--- src/main/SkiplistPrepareData.java
+++ src/main/SkiplistPrepareData.java
@@ -4,22 +4,12 @@ import java.util.ArrayList;
 
 public class SkiplistPrepareData {
 	//Prepares index arrays of each letter for a given text.
-	int[] skiplistB;
-	int[] skiplistD;
-	int[] skiplistF;
-	int[] skiplistZ;
-
 	ArrayList<Integer> poslistB = new ArrayList<Integer>();
 	ArrayList<Integer> poslistD = new ArrayList<Integer>();
 	ArrayList<Integer> poslistF = new ArrayList<Integer>();
 	ArrayList<Integer> poslistZ = new ArrayList<Integer>();
 
 	public void getSkiplists(String text) {
-		skiplistB = new int[text.length()];
-		skiplistD = new int[text.length()];
-		skiplistF = new int[text.length()];
-		skiplistZ = new int[text.length()];
-
 		for (int i = 0; i < text.length(); i++) {
 
 			switch (text.charAt(i)) {
@@ -41,20 +31,6 @@ public class SkiplistPrepareData {
 		}
 	}
 
-	public int[] getSkiplist(char a) {
-		switch (a) {
-		case 'B':
-			return skiplistB;
-		case 'D':
-			return skiplistD;
-		case 'F':
-			return skiplistF;
-		case 'Z':
-			return skiplistZ;
-		default:
-			return null;
-		}
-	}
 
 	public ArrayList<Integer> getPosList(char a) {
 		switch (a) {
