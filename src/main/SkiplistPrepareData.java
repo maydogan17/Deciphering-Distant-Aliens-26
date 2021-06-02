d026eb64506e35e9facd2e89544f68661306294f
Gülay Canbaloğlu <60276129+notreculi@users.noreply.github.com>
2021-06-02 23:24:06 +0300

    feat(core): removal of unused parts

--- src/main/SkiplistPrepareData.java
+++ src/main/SkiplistPrepareData.java
@@ -3,23 +3,13 @@ package main;
 import java.util.ArrayList;
 
 public class SkiplistPrepareData {
-	//Prepares index arrays of each letter for a given text.
-	int[] skiplistB;
-	int[] skiplistD;
-	int[] skiplistF;
-	int[] skiplistZ;
-
+	//Prepares position lists of each letter for a given text.
 	ArrayList<Integer> poslistB = new ArrayList<Integer>();
 	ArrayList<Integer> poslistD = new ArrayList<Integer>();
 	ArrayList<Integer> poslistF = new ArrayList<Integer>();
 	ArrayList<Integer> poslistZ = new ArrayList<Integer>();
 
 	public void getSkiplists(String text) {
-		skiplistB = new int[text.length()];
-		skiplistD = new int[text.length()];
-		skiplistF = new int[text.length()];
-		skiplistZ = new int[text.length()];
-
 		for (int i = 0; i < text.length(); i++) {
 
 			switch (text.charAt(i)) {
@@ -38,21 +28,6 @@ public class SkiplistPrepareData {
 			default:
 				break;
 			}
-		}
-	}
-
-	public int[] getSkiplist(char a) {
-		switch (a) {
-		case 'B':
-			return skiplistB;
-		case 'D':
-			return skiplistD;
-		case 'F':
-			return skiplistF;
-		case 'Z':
-			return skiplistZ;
-		default:
-			return null;
 		}
 	}
 
